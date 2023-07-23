3723319d154cc68cd4421db28916c55e3fd24eb5
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2023-07-23 19:11:49 +0300

    Delete CSharpWebAdvanced-CourceProject-June2023 directory

--- CSharpWebAdvanced-CourceProject-June2023/HouseRentingSystem.Web.ViewModels/Home/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HouseRentingSystem.Web.ViewModels.Home
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
