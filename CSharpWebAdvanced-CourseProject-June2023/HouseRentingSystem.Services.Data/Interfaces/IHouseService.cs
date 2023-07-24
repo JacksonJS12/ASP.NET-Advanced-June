0d417f23e1a3a59a018780e7b7db252b29276c2f
Deniz <d.ivan0v.6100@gmail.com>
2023-07-24 16:25:59 +0300

    add house is done

--- CSharpWebAdvanced-CourseProject-June2023/HouseRentingSystem.Services.Data/Interfaces/IHouseService.cs
+++ CSharpWebAdvanced-CourseProject-June2023/HouseRentingSystem.Services.Data/Interfaces/IHouseService.cs
@@ -1,9 +1,12 @@
 ﻿namespace HouseRentingSystem.Services.Data.Interfaces
 {
+    using HouseRentingSystem.Web.ViewModels.House;
     using Web.ViewModels.Home;
 
     public interface IHouseService
     {
         Task<IEnumerable<IndexViewModel>> LastThreeHousesAsync();
+
+        Task CreateAsync(HouseFormModel formModel, string agentId);
     }
 }
